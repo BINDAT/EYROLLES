f431106c643879a15eaf46494f0711a6c575743c
bindat <maximerigaud66@gmail.com>
2026-04-17 15:02:42 +0200

    Ajout de la classe SayController avec la méthode hello

--- Ruby/annexe ruby/démo.rb
+++ Ruby/annexe ruby/démo.rb
@@ -1,9 +1,9 @@
-class Project < ActiveRecord::Base
-    belong_to               :portfolio
-    has_one                 :project_manager
-    has_many                :milestones
-    has_many                :deliverables, :through => : milestones
-    validates_presence_of   :name, :description
-    validates_acceptance_of :non_disclosure_agreement
-    validates_uniqueness_of :short_name
-end
+class Project < ActiveRecord::Base #Définition de la classe Project qui hérite de ActiveRecord::Base
+    belong_to               :portfolio #Indique que chaque projet appartient à un portfolio
+    has_one                 :project_manager #Indique que chaque projet a un project manager
+    has_many                :milestones #Indique que chaque projet peut avoir plusieurs milestones
+    has_many                :deliverables, :through => : milestones #Indique que chaque projet peut avoir plusieurs deliverables à travers les milestones
+    validates_presence_of   :name, :description #Valide la présence des attributs name et description
+    validates_acceptance_of :non_disclosure_agreement #Valide que l'accord de non-divulgation a été accepté
+    validates_uniqueness_of :short_name #Valide que le short_name est unique
+end #Fin de la classe Project
---
+++ Ruby/annexe ruby/say_controller.rb
@@ -0,0 +1,4 @@
+class SayController < ApplicationController #Héritage de la classe ApplicationController
+  def hello #Définition de la méthode hello
+  end #Fin de la méthode hello
+end #Fin de la classe SayController
