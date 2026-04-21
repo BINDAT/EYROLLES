ba1ce0caf9f3d9b4d5d87364093431693567f7d0
bindat <maximerigaud66@gmail.com>
2026-04-21 22:09:57 +0200

    Ajout de la méthode update_sales_tax pour mettre à jour les taxes des commandes

---
+++ Ruby/annexe ruby/update_sales_tax.rb
@@ -0,0 +1,7 @@
+def update_sales_tax
+  update = @db.prepare("update orders set tax=? where id=?")
+  @db.select_all("select id, amount from orders") do |id, amount|
+    tax = calc_sales_tax(amount)
+    update.execute(tax, id)
+  end
+end
